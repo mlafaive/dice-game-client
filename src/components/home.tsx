43a3cd359dbe3a53e26a46247933f4fbe771b201
mlafaive <mlafaive@umich.edu>
2021-01-07 17:21:26 -0800

    added bootstrap and home and landing pages

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Paths } from '../constants'; 
 
 export default function Home() {
   return (
-    <div>home</div>
+    <Container>
+      <Row className='min-vh-100 align-items-center'>
+        <Col>
+          <Row className='justify-content-center text-center'>
+            <h1>Welcome to Dice Game!</h1>
+          </Row>
+          <Row className='justify-content-center'>
+            <LinkContainer to={Paths.NewGame}>
+              <Button>Create new game</Button>
+            </LinkContainer>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- src/components/new-game.tsx
+++ src/components/new-game.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export default function NewGame() {
+  const [gameName, setGameName] = useState('');
+  const [playerName, setPlayerName] = useState('');
   return (
-    <div>new game</div>
+    <Container>
+      <Row className='min-vh-100 align-items-center'>
+        <Col>
+          <Row className='justify-content-center'>
+            <h1>Create new game</h1>
+          </Row>
+          <Row className='justify-content-center text-center'>
+            <Form>
+              <Form.Group controlId='gameName' className='text-left'>
+                <Form.Label>Game name</Form.Label>
+                <Form.Control value={gameName} onChange={e => setGameName(e.target.value)}/>
+              </Form.Group>
+
+              <Form.Group controlId='playerName' className='text-left'>
+                <Form.Label>Player name</Form.Label>
+                <Form.Control value={playerName} onChange={e => setPlayerName(e.target.value)}/>
+              </Form.Group>
+
+              <Button variant="primary" onClick={() => console.log(`creating game ${gameName} with player ${playerName}`)}>
+                Create game
+              </Button>
+            </Form>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
